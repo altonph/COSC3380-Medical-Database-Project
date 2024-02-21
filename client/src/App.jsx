0ca53f582af9690b48b5714ab2ba99377cb95449
Alton Phan <altonphan@outlook.com>
2024-02-21 17:50:37 -0600

    install preline ui library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
+import "preline/preline";
 
+//for testing, will be adjusted
 function App() {
-  //const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className='text-3xl'>Shasta Dental</h1>
       <p className='bg-red-500'>tailwind works :D</p>
+      <button type='button'
+        className='py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'>
+        Preline testing button
+      </button>
     </>
   )
 }
 
 export default App
+
