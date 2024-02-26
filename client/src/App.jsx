2654e8944f84c83b2ede8679c820d5777b228188
Alton Phan <altonphan@outlook.com>
2024-02-25 19:15:02 -0600

    add new imports and configure files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import "preline/preline";
 
 //for testing, will be adjusted
 function App() {
   return (
     <>
-      <h1 className='text-3xl'>Shasta Dental</h1>
-      <p className='bg-red-500'>tailwind works :D</p>
-      <button type='button'
-        className='py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'>
-        Preline testing button
-      </button>
+      <div className="mx-auto my-auto h-screen bg-white text-black">
+        <Router>
+          <Routes>
+            <Route index element= {<LandingPage />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   )
-}
+};
 
 export default App
 
