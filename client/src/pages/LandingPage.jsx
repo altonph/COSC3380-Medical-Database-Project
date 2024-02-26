df57ab43445dd9a7f48dfa00019244c9c2d098dc
Alton Phan <altonphan@outlook.com>
2024-02-25 19:19:19 -0600

    test dev branch with LandingPage title

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const LandingPage = () => {
     return (
         <>
-        
+            <h1 className="text-3xl">Shasta Dental</h1>
         </>
     )
 };
