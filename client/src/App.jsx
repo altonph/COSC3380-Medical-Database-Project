4b3c2f83480494b208e9ac3b52891bf1e566f2a5
Marshall Morgan <marshallhmorgan@gmail.com>
2024-03-11 15:25:40 -0500

    edited styling and forms of login and registerpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import TestPage from "./pages/TestPage";
-import LoginPage from "./pages/LoginPage"; // Import LoginPage component
-import RegisterPage from "./pages/RegisterPage"; // Import RegisterPage component
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import "preline/preline";
 
-//for testing, will be adjusted
 function App() {
   return (
-    <>
-      <div className="mx-auto my-auto h-screen bg-white text-black">
-        <Router>
-          <Routes>
-            <Route index element= {<LandingPage />} />
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} /> {/* Add LoginPage route */}
-            <Route path="/register" element={<RegisterPage />} /> {/* Add RegisterPage route */}
-            <Route path="/TestPage" element={<TestPage />} />
-          </Routes>
-        </Router>
-      </div>
-    </>
-  )
-};
+    <div className="mx-auto my-auto h-screen bg-white text-black">
+      <Router>
+        <Routes>
+          <Route index element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/test" element={<TestPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
-export default App
-
+export default App;
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,10 +6,14 @@ const RegisterPage = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
+    const [gender, setGender] = useState('');
+    const [dob, setDob] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [address, setAddress] = useState('');
 
     const handleSubmit = (e) => { 
         e.preventDefault();
-        console.log(email);
+        console.log(email, password, name, gender, dob, phoneNumber, address);
     }
 
     return (
@@ -23,6 +27,25 @@ const RegisterPage = (props) => {
                     placeholder="Full Name" 
                     id="name" 
                     name="name"
+                />
+                <label htmlFor="gender">Gender</label>
+                <select 
+                    value={gender} 
+                    onChange={(e) => setGender(e.target.value)} 
+                    id="gender" 
+                    name="gender"
+                >
+                    <option value="">Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                <label htmlFor="dob">Date of Birth</label>
+                <input 
+                    value={dob} 
+                    onChange={(e) => setDob(e.target.value)} 
+                    type="date" 
+                    id="dob" 
+                    name="dob"
                 />
                 <label htmlFor="email">Email</label>
                 <input 
@@ -41,6 +64,23 @@ const RegisterPage = (props) => {
                     placeholder="********" 
                     id="password" 
                     name="password"
+                />
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input 
+                    value={phoneNumber} 
+                    onChange={(e) => setPhoneNumber(e.target.value)} 
+                    type="tel" 
+                    placeholder="Phone Number" 
+                    id="phoneNumber" 
+                    name="phoneNumber"
+                />
+                <label htmlFor="address">Address</label>
+                <textarea 
+                    value={address} 
+                    onChange={(e) => setAddress(e.target.value)} 
+                    placeholder="Address" 
+                    id="address" 
+                    name="address"
                 />
                 <button type="submit" className="register-button">Register</button>
             </form>
