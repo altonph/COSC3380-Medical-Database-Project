fd7af91f4bd3aa1fba540be96d0e00c6615113c1
Shadha-K <kshadha@gmail.com>
2024-03-14 13:18:37 -0500

    Added Patient Profile page

--- client/src/pages/patient/PatientProfile.jsx
+++ client/src/pages/patient/PatientProfile.jsx
@@ -3,8 +3,6 @@ import HeaderPortalPatient from "../../components/HeaderPortalPatient";
 import { Link } from 'react-router-dom'; 
 
 const PatientProfile = () => {
-
-  // Default values
   const defaultValues = {
     firstName: 'Random',
     lastName: 'Patient',
@@ -33,7 +31,6 @@ const PatientProfile = () => {
             <div className="border border-gray-300 rounded-md py-2 px-3">{defaultValues.lastName}</div>
           </div>
 
-          {/* Rest of the form fields */}
           <div>
           <label className="block mb-2">Gender:</label>
             <div className="border border-gray-300 rounded-md py-2 px-3">{defaultValues.gender}</div>
