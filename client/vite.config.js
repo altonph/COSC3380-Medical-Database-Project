4bf5c2e340d4d5b4684aced12ace3c44121edc7a
Alton Phan <altonphan@outlook.com>
2024-03-24 01:59:56 -0500

    testing page vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,29 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  build: {
+    rollupOptions: {
+      input: {
+        page1: './src/pages/LandingPage.jsx',
+        page2: './src/pages/ContactPage.jsx',
+        page3: './src/pages/AboutPage.jsx'
+        // Add more entry points as needed
+      },
     },
   },
-})
+});
+
+
