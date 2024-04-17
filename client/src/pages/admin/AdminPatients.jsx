38724f797f1419550aea8e4578384c031284f033
Alton Phan <altonphan@outlook.com>
2024-04-16 22:16:23 -0500

    frontend patient overview ready for fetch to edit

--- client/src/pages/admin/AdminPatients.jsx
+++ client/src/pages/admin/AdminPatients.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import HeaderPortalAdmin from "../../components/HeaderPortalAdmin";
 import Footer from "../../components/Footer";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AdminPatients = () => {
     const [patients, setPatients] = useState([]);
+    const [editedIndex, setEditedIndex] = useState(null);
 
     useEffect(() => {
         const fetchPatients = async () => {
@@ -22,6 +25,37 @@ const AdminPatients = () => {
 
         fetchPatients();
     }, []);
+
+    const handleEdit = (index) => {
+        setEditedIndex(index);
+    };
+
+    const handleCancelEdit = async () => {
+        setEditedIndex(null);
+    // Refetch patient data
+        try {
+            const response = await fetch("http://localhost:5000/api/admin/getPatients");
+            if (response.ok) {
+                const data = await response.json();
+                setPatients(data);
+            } else {
+                console.error("Failed to fetch patients");
+            }
+        } catch (error) {
+            console.error("Error fetching patients:", error);
+        }
+    };
+
+    const handleInputChange = (date, index) => {
+        setPatients(prevPatients => {
+            return prevPatients.map((patient, i) => {
+                if (i === index) {
+                    return { ...patient, DOB: date };
+                }
+                return patient;
+            });
+        });
+    };
 
     return (
         <>
@@ -66,25 +100,66 @@ const AdminPatients = () => {
                                         <th className="px-4 py-2 text-sm">Email</th>
                                         <th className="px-4 py-2 text-sm">Phone Number</th>
                                         <th className="px-4 py-2 text-sm">Address</th>
+                                        <th className="px-4 py-2 text-sm">Is Active</th>
                                         <th className="px-4 py-2 text-sm">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {patients.map(patient => (
+                                    {patients.map((patient, index) => (
                                         <tr key={patient.patientID}>
                                             <td className="border px-4 py-2">{patient.patientID}</td>
-                                            <td className="border px-4 py-2 ">{patient.Policy_number}</td>
-                                            <td className="border px-4 py-2">{patient.Insurance_Company_Name}</td>
-                                            <td className="border px-4 py-2">{patient.Gender}</td>
-                                            <td className="border px-4 py-2">{patient.FName}</td>
-                                            <td className="border px-4 py-2">{patient.LName}</td>
-                                            <td className="border px-4 py-2">{new Date(patient.DOB).toLocaleDateString()}</td>
-                                            <td className="border px-4 py-2">{patient.Email}</td>
-                                            <td className="border px-4 py-2">{patient.Phone_num}</td>
-                                            <td className="border px-4 py-2">{patient.Address}</td>
+                                            <td className="border px-4 py-2">{editedIndex === index ? <input type="text" name="Policy_number" value={patient.Policy_number} onChange={(e) => handleInputChange(e, index)} /> : patient.Policy_number}</td>
                                             <td className="border px-4 py-2">
-                                                <button className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:ring focus:border-blue-300">Edit</button>
-                                                <button className="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:ring focus:border-red-300 ml-2">Delete</button>
+                                                {editedIndex === index ? (
+                                                    <select name="Insurance_Company_Name" value={patient.Insurance_Company_Name} onChange={(e) => handleInputChange(e, index)}>
+                                                        <option value="">Select</option>
+                                                        <option value="Anthem">Anthem</option>
+                                                        <option value="Guardian">Guardian</option>
+                                                        <option value="Ameritas">Ameritas</option>
+                                                        <option value="Humana">Humana</option>
+                                                        <option value="Spirit Dental">Spirit Dental</option>
+                                                    </select>
+                                                ) : (
+                                                    patient.Insurance_Company_Name
+                                                )}
+                                            </td>
+                                            <td className="border px-4 py-2">
+                                                {editedIndex === index ? (
+                                                    <select name="Gender" value={patient.Gender} onChange={(e) => handleInputChange(e, index)}>
+                                                        <option value="">Select</option>
+                                                        <option value="Male">Male</option>
+                                                        <option value="Female">Female</option>
+                                                    </select>
+                                                ) : (
+                                                    patient.Gender
+                                                )}
+                                            </td>
+                                            <td className="border px-4 py-2">{editedIndex === index ? <input type="text" name="FName" value={patient.FName} onChange={(e) => handleInputChange(e, index)} /> : patient.FName}</td>
+                                            <td className="border px-4 py-2">{editedIndex === index ? <input type="text" name="LName" value={patient.LName} onChange={(e) => handleInputChange(e, index)} /> : patient.LName}</td>
+                                            <td className="border px-4 py-2">
+                                                {editedIndex === index ? (
+                                                    <DatePicker
+                                                        selected={new Date(patient.DOB)}
+                                                        onChange={(date) => handleInputChange(date, index)}
+                                                        dateFormat="MM/dd/yyyy"
+                                                    />
+                                                ) : (
+                                                    new Date(patient.DOB).toLocaleDateString()
+                                                )}
+                                            </td>
+                                            <td className="border px-4 py-2">{editedIndex === index ? <input type="text" name="Email" value={patient.Email} onChange={(e) => handleInputChange(e, index)} /> : patient.Email}</td>
+                                            <td className="border px-4 py-2">{editedIndex === index ? <input type="text" name="Phone_num" value={patient.Phone_num} onChange={(e) => handleInputChange(e, index)} /> : patient.Phone_num}</td>
+                                            <td className="border px-4 py-2">{editedIndex === index ? <input type="text" name="Address" value={patient.Address} onChange={(e) => handleInputChange(e, index)} /> : patient.Address}</td>
+                                            <td className="border px-4 py-2">{patient.is_active === 1 ? "Active" : "Inactive"}</td>
+                                            <td className="border px-4 py-2">
+                                                {editedIndex === index ? (
+                                                    <>
+                                                        <button onClick={() => handleEdit(null)} className="bg-green-500 hover:bg-green-700 text-white py-1 px-2 rounded focus:outline-none focus:ring focus:border-green-300">Confirm</button>
+                                                        <button onClick={handleCancelEdit} className="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:ring focus:border-red-300 ml-2">Cancel</button>
+                                                    </>
+                                                ) : (
+                                                    <button onClick={() => handleEdit(index)} className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:ring focus:border-blue-300">Edit</button>
+                                                )}
                                             </td>
                                         </tr>
                                     ))}
